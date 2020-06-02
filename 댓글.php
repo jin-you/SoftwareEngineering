9e9cf4fb77d4d7fdf7f0325203e3c74009eb7ebc
ye0nz <63764167+ye0nz@users.noreply.github.com>
2020-06-03 07:03:26 +0900

    댓글기능(참고)

---
+++ 댓글.php
@@ -0,0 +1,36 @@
+<form method="post" action="/comment/add/82" onsubmit="return false" style="margin: 0">
+                        <fieldset class="fld_reply">
+                          <legend class="screen_out">댓글쓰기 폼</legend>
+                          <div class="reply_write ">
+                            <label for="comment" class="lab_write"></label>
+                            <textarea id="comment" name="comment" class="tf_reply" placeholder="여러분의 소중한 댓글을 입력해주세요" tabindex="1"></textarea>
+                          </div>
+                          <div class="reply_writer">
+
+
+                                <div class="writer_info">
+                                  <span class="info_name ">
+                                    <span class="wrap_info">
+                                      <input type="text" name="name" id="name" class="inp_info" placeholder="이름" value="" tabindex="2">
+                                    </span>
+                                  </span>
+                                  <span class="info_pw ">
+                                    <span class="wrap_info">
+                                      <input type="password" name="password" id="password" class="inp_info" placeholder="비밀번호" tabindex="3">
+                                    </span>
+                                  </span>
+                                </div>
+
+
+                            <div class="writer_btn">
+                              <button type="submit" class="btn_enter" onclick="addComment(this, 82); return false;" tabindex="5">입력</button>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </form>
+
+                    </div><script type="text/javascript">loadedComments[82]=true;findFragmentAndHighlight(82)</script>
+                  </div>
+                </div>
+
+            </div>
