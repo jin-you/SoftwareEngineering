a71b267b1d033f0267e439cb1d465fccbc75e585
jin-you <58108092+jin-you@users.noreply.github.com>
2020-06-04 00:31:37 +0900

    Update search_result.php

--- Hankki/search/search_result.php
+++ Hankki/search/search_result.php
@@ -16,8 +16,8 @@
 		
 		
 		echo "<tr>";
-		echo "<td><a href='recipe_view.php?recipeID=$item_num'><img src='$item_image' width='250' height='250' class='img-responsive'></a></td>";
-		echo "<td><a href='recipe_view.php?recipeID=$item_num'>$item_subject</a></td>";
+		echo "<td><a href='../board/recipe_view.php?recipeID=$item_num'><img src='$item_image' width='250' height='250' class='img-responsive'></a></td>";
+		echo "<td><a href='../board/recipe_view.php?recipeID=$item_num'>$item_subject</a></td>";
 		echo "<td>$item_id</td>";
 		echo "<td>$item_score</td>";
 		echo "<td>$item_date</td>";
